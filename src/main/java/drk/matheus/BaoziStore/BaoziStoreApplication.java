667c1307217f758c38a649b5a48a62e4fb0fa0e3
drkmatheus <matheus.gcrosciati@gmail.com>
2026-06-08 11:55:32 -0300

    [drkmatheus] Adjustments and test endpoint implemented

--- src/main/java/drk/matheus/BaoziStore/BaoziStoreApplication.java
+++ src/main/java/drk/matheus/BaoziStore/BaoziStoreApplication.java
@@ -2,12 +2,18 @@ package drk.matheus.BaoziStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BaoziStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaoziStoreApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String status() { return "UP!"; }
 
 }
